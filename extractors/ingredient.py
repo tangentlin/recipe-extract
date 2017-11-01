a09673326665a69d5174eecff5fd1c2347161e83
Tangent Lin <tangent@usa.net>
2017-11-01 19:08:03 -0400

    Scaffold code to process PDF or JPG/PNG images

--- extractors/ingredient.py
+++ extractors/ingredient.py
@@ -6,6 +6,12 @@ import os
 import tempfile
 import subprocess
 from ingredient_phrase_tagger.training import utils
+from cStringIO import StringIO
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
 
 
 class IngredientModel(object):
@@ -39,7 +45,6 @@ class IngredientExtractor(object):
 
         return models
 
-
     def get_crf_output(self, text):
         _, cleansed_file = tempfile.mkstemp()
         with open(cleansed_file, "w") as out_file:
@@ -48,4 +53,29 @@ class IngredientExtractor(object):
         command = "crf_test -v 1 -m %s %s" % (self.model_file, cleansed_file)
         proc = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
         (crf_out, err) = proc.communicate()
+        os.remove(cleansed_file)
         return crf_out.decode("utf-8")
+
+    def extract_pdf(self, pdf_file):
+        rsrcmgr = PDFResourceManager()
+        retstr = StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        laparams.all_texts = True
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        fp = open(pdf_file, 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        password = ""
+        maxpages = 0
+        caching = True
+        pagenos = set()
+        pages = PDFPage.get_pages(
+            fp, pagenos, maxpages=maxpages, password=password,
+            caching=caching, check_extractable=True)
+        for page in pages:
+            interpreter.process_page(page)
+        fp.close()
+        device.close()
+        str = retstr.getvalue()
+        retstr.close()
+        return self.extract_text(str)
